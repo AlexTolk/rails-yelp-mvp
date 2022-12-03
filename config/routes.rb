d0c491da2c7f838108a79b67392125c317916296
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2022-12-03 17:17:07 +0800

    created two pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  get 'restaurants/:id/reviews/new', to: 'reviews#new'
+  post 'restaurants/:id/reviews', to: 'reviews#create'
 end
